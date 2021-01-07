cfd57fe01eecda985d875b8b681e530403b6f2b3
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-07 15:20:44 -0600

    dashed line

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ franklin.color("green")
 franklin.shape("turtle")
 
 #making square
-franklin.forward(45)
-franklin.right(90)
-franklin.forward(45)
-franklin.right(90)
-franklin.forward(45)
-franklin.right(90)
-franklin.forward(45)
+# franklin.forward(45)
+# franklin.right(90)
+# franklin.forward(45)
+# franklin.right(90)
+# franklin.forward(45)
+# franklin.right(90)
+# franklin.forward(45)
+
+#Drawing dashed line
+
+for i in range(50):
+    franklin.forward(10)
+    franklin.penup()
+    franklin.forward(10)
+    franklin.pendown()
+    franklin.forward(10)
+    franklin.penup()
+    franklin.forward(10)
+    # franklin.penup()
+    # franklin.pendown()
+    # franklin.penup()
 
 
 
