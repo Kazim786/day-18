960d0e3e2b91d340443aea3ae538ba2b0ab27ea7
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-07 14:50:33 -0600

    square made

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from turtle import Turtle
-
+from turtle import Turtle, Screen
 franklin = Turtle()
+franklin.color("green")
+franklin.shape("turtle")
+
+#making square
+franklin.forward(45)
+franklin.right(90)
+franklin.forward(45)
+franklin.right(90)
+franklin.forward(45)
+franklin.right(90)
+franklin.forward(45)
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
