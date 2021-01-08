adb4eecfe2a9b1d36c3c8471ef3e10130dcd0098
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-07 21:18:06 -0600

    function working

--- main.py
+++ main.py
@@ -14,22 +14,68 @@ franklin.shape("turtle")
 
 #Drawing dashed line
 
-for i in range(50):
-    franklin.forward(10)
-    franklin.penup()
-    franklin.forward(10)
-    franklin.pendown()
-    franklin.forward(10)
-    franklin.penup()
-    franklin.forward(10)
-    # franklin.penup()
-    # franklin.pendown()
-    # franklin.penup()
+# for i in range(50):
+#     franklin.forward(10)
+#     franklin.penup()
+#     franklin.forward(10)
+#     franklin.pendown()
+#     franklin.forward(10)
+#     franklin.penup()
+#     franklin.forward(10)
+  
+
+#Making shapes
+#triangle
+# franklin.forward(120)
+# franklin.right(120)
+# franklin.forward(120)
+# franklin.right(120)
+# franklin.forward(120)
+# #square
+# franklin.forward(45)
+# franklin.right(90)
+# franklin.forward(45)
+# franklin.right(90)
+# franklin.forward(45)
+# franklin.right(90)
+# franklin.forward(45)
+# #pentagon
+# franklin.forward(72)
+# franklin.right(72)
+# franklin.forward(72)
+# franklin.right(72)
+# franklin.forward(72)
+# franklin.right(72)
+# franklin.forward(72)
+
+# #hexagon
+# franklin.forward(60)
+# franklin.right(60)
+# franklin.forward(60)
+# franklin.right(60)
+# franklin.forward(60)
+# franklin.right(60)
+# franklin.forward(60)
+# franklin.right(60)
+# franklin.forward(60)
+# franklin.right(60)
+# franklin.right(60)
+# franklin.forward(60)
+# franklin.right(60)
 
 
+#Making shapes but using a function - what angela did
+# Since the number of sides wont be the same for all the shapes we will use a function 
+def draw_shape (num_sides):
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        franklin.forward(100)
+        franklin.right(angle)
 
 
-
+draw_shape(3)
+draw_shape(4)
+draw_shape(5)
 
 
 screen = Screen()
